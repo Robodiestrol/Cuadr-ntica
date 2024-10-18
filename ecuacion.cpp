c9f72ed92b92fdd2ace9fd20d56651d7101244ac
Robodiestrol <yaellg08@gmail.com>
2024-10-17 19:15:18 -0600

    version_final

--- ecuacion.cpp
+++ ecuacion.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
   double a, b, c;
   double x, xx;
-  cout<<"Ingrese la vaariable de de A, B, C de la operacion"<<endl;
+  cout<<"Este programa realisa ecuaciones cuadranticas, para lo cual primero lee los valores de A, B, C. luego de eso realisa la ecuasion dando haci el resultado de x1 y x2"<<endl;
   cin >> a >> b >> c;
   if (a == 0){
     cout << "Se a detectado un error";
@@ -12,9 +12,7 @@ int main() {
       
     x=(-b-sqrt(b*b-4*a*c))/(2*a);
     xx=(-b+sqrt(b*b-4*a*c))/(2*a);
-    cout<<"El resultado de x1 es: "<<endl;
     printf("%.2f\n", x);
-    cout<<"El resultado de x2 es: "<<endl;
     printf("%.2f\n", xx);
     
   }
