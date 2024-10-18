0d6acd71e833a6453b9e30c5b77280d6e5472337
Robodiestrol <yaellg08@gmail.com>
2024-10-17 18:54:39 -0600

    ver2

--- ecuacion.cpp
+++ ecuacion.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
   double a, b, c;
   double x, xx;
+  cout<<"Ingrese la vaariable de de A, B, C de la operacion"<<endl;
   cin >> a >> b >> c;
   if (a == 0){
     cout << "Se a detectado un error";
@@ -11,7 +12,9 @@ int main() {
       
     x=(-b-sqrt(b*b-4*a*c))/(2*a);
     xx=(-b+sqrt(b*b-4*a*c))/(2*a);
+    cout<<"El resultado de x1 es: "<<endl;
     printf("%.2f\n", x);
+    cout<<"El resultado de x2 es: "<<endl;
     printf("%.2f\n", xx);
     
   }
